e4f675ac87c46ccde399a57e411e8359328dc314
Simbeul <64574406+Simbeul@users.noreply.github.com>
2021-10-12 15:44:46 -0700

    Updated main script

--- script.py
+++ script.py
@@ -9,8 +9,14 @@ from time import sleep
 list = ['WAGMI', 'Lets get this Level 10 boys', 'Love this project', 'LFG!!', 'Up all night for this LVL10', 'I love this project','Great community', 'Love this chat', 'This is sick', 'Cant wait to be level 10', 'Lots of people grinfing... respect', 'were all in this together','LETS GOOO', 'This is taking a while','moon time', 'LFG Guys', 'Yoooo', 'To the mooooon', 'Bears are lit', 'We love this project', 'So many messages lol']
 print('Bot launched. Click on desktop.')
 time.sleep(5)
-a = 1
-while a == 1:
+Count = 0
+XPGains = 0
+
+while True:
     pyautogui.typewrite(random2.choice(list))
-    time.sleep(randint(31,40))
-    pyautogui.press('enter')  
+    pyautogui.press('enter')
+    Count +=1
+    XPGains +=10
+    print('Message #'+ str(Count) +'. Roughly '+ str(XPGains) +'XP gained.')
+    time.sleep(randint(70,75))
+    
