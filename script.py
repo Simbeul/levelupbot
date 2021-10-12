5e7df703d199ae955ed5646972b4a907e6c8bbcb
Simbeul <64574406+Simbeul@users.noreply.github.com>
2021-10-12 15:46:03 -0700

    Update on main

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ import pyautogui
 from random import randint
 from time import sleep
 list = ['WAGMI', 'Lets get this Level 10 boys', 'Love this project', 'LFG!!', 'Up all night for this LVL10', 'I love this project','Great community', 'Love this chat', 'This is sick', 'Cant wait to be level 10', 'Lots of people grinfing... respect', 'were all in this together','LETS GOOO', 'This is taking a while','moon time', 'LFG Guys', 'Yoooo', 'To the mooooon', 'Bears are lit', 'We love this project', 'So many messages lol']
-print('Bot launched. Click on desktop.')
+print('Bot launched. You have 5 seconds to click on the desired discord group channel, on discord.com')
 time.sleep(5)
 Count = 0
 XPGains = 0
@@ -17,6 +17,6 @@ while True:
     pyautogui.press('enter')
     Count +=1
     XPGains +=10
-    print('Message #'+ str(Count) +'. Roughly '+ str(XPGains) +'XP gained.')
+    print('Message #'+ str(Count) +' sent. Thats roughly '+ str(XPGains) +'XP gained.')
     time.sleep(randint(70,75))
     
